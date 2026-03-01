6652ee0c7fcd18edb490031488a2c77a3ab1ba57
cracksre <111478253+cracksre@users.noreply.github.com>
2026-03-01 10:54:38 -0600

    Corrected syntax for r and w within file operns

--- File_Operations/update_server.py
+++ File_Operations/update_server.py
@@ -1,9 +1,9 @@
 def update_server_config(file_path,key,value):
     
-    with open(file_path, 'r') as file:
+    with open(file_path, "r") as file:
         lines = file.readlines()
     
-    with open(file_path,'w') as file:
+    with open(file_path,"w") as file:
         for line in lines:
             if key in line:
                 file.write(key + "=" + value + "\n")
